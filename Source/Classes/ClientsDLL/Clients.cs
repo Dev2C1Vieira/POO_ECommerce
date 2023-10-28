822f001772140797f9c09861f25722480b788562
pedro-25626-vieira <pedrohcvieira.seguranca.2004@gmail.com>
2023-10-28 18:28:26 +0100

    Project structuring. Creation of Classes. Creation of attributes and their respective methods in the Clients class. Creation of the project to request and write data to the console, InputConsoleOutput.

--- Source/Classes/ClientsDLL/Clients.cs
+++ Source/Classes/ClientsDLL/Clients.cs
@@ -9,6 +9,7 @@
  * */
 
 using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ClientsDLL
 {
@@ -20,16 +21,16 @@ namespace ClientsDLL
     /// </summary>
     public class Clients
     {
-        /// <summary>
-        /// 
-        /// </summary>
         #region Attributes
 
+        /// <summary>
+        /// Creation of the Clients class atributes
+        /// </summary>
         private string name;
         private DateTime birth_date;
-        private bool gender;
+        private int gender;
         private string address;
-        private int ph_number;
+        private int phone_number;
         private string email_address;
 
         private static int Qtd_Clients;
@@ -41,29 +42,145 @@ namespace ClientsDLL
         #region Constructors
 
         /// <summary>
-        /// 
+        /// Default constructor
         /// </summary>
         public Clients()
         {
             name = "";
-            gender = true;
+            gender = -1;
             address = "";
-            ph_number = -1;
+            phone_number = -1;
             email_address = "";
             Qtd_Clients = 0;
         }
 
-        public Clients()
+        /// <summary>
+        /// Constructor passed by parameters
+        /// </summary>
+        /// <param name="External_name"></param>
+        /// <param name="External_gender"></param>
+        /// <param name="External_address"></param>
+        /// <param name="External_phone_number"></param>
+        /// <param name="External_email_address"></param>
+        public Clients(string External_name, int External_gender, string External_address,
+            int External_phone_number, string External_email_address)
         {
-
+            this.name = External_name;
+            this.gender = External_gender;
+            this.address = External_address;
+            this.phone_number = External_phone_number;
+            this.email_address = External_email_address;
         }
 
         #endregion
 
         #region Properties
+        /// <summary>
+        /// Property related to the name attribute
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// Property related to the gender attribute
+        /// </summary>
+        public int Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        /// <summary>
+        /// Property related to the address attribute
+        /// </summary>
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        /// <summary>
+        /// Property related to the phone_number attribute
+        /// </summary>
+        public int PhoneNumber
+        {
+            get { return phone_number; }
+            set { phone_number = value; }
+        }
+
+        /// <summary>
+        /// Property related to the email_address attribute
+        /// </summary>
+        public string EmailAddress
+        {
+            get { return email_address; }
+            set { email_address = value; }
+        }
+
+        #endregion
+
+        #region Operators
+
+        /// <summary>
+        /// Creating/Rewriting this method, to be able to compare 2 different objects.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(Clients left, Clients right) 
+        {
+            if ((left.Name == right.Name) && (left.Gender == right.Gender) 
+                && (left.Address == right.Address) && (left.PhoneNumber == right.PhoneNumber) 
+                && (left.EmailAddress == right.EmailAddress)) return (true);
+            return (false);
+
+        }
+
+        /// <summary>
+        /// Creating/Rewriting this method, to be able to compare 2 different objects.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(Clients left, Clients right)
+        {
+            if (!(left == right)) return (true);
+            return (false);
+        }
+
         #endregion
 
         #region Overrides
+
+        /// <summary>
+        /// Rewriting the ToString method, to be able to write on the console.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return (String.Format("Name: {0} - Gender: {1} - Address: {2} - Phone Number: {3} " +
+                "- Email Address: {4}", Name, Gender.ToString(), Address, PhoneNumber.ToString(), EmailAddress));
+        }
+
+        /// <summary>
+        /// Rewriting the Equals method, to be able to compare 2 different objects.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Clients)
+            {
+                Clients client = (Clients)obj;
+                if (this == client)
+                    return (true);
+            }
+            return (false);
+        }
+
         #endregion
 
         #region OtherMethods
---
+++ Source/Input_Output/InputConsoleOutput/IO.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * <copyright file = "IO.cs" company = "IPCA">
+ * Copyright (c) 2023 All Rights Reserved
+ * </copyright>
+ * <author> Pedro Vieira </author>
+ * <date> 10/28/2023 6:21:34 PM </date>
+ * <description> [Write the description of the project!] </description>
+ * 
+ * */
+
+using System;
+
+namespace InputConsoleOutput
+{
+    /// <summary>
+    /// Purpose: [Write the purpose of the class!]
+    /// Created By: Pedro Vieira
+    /// Created On: 10/28/2023 6:21:34 PM
+    /// Email: a25626@alunos.ipca.pt
+    /// </summary>
+    public class IO
+    {
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public IO()
+        {
+
+        }
+
+        #endregion
+
+        #region OtherMethods
+
+        #region ClientsFunctions
+
+
+
+        #endregion
+
+        #endregion
+
+        #region Destructor
+        #endregion
+
+        #endregion
+    }
+}
